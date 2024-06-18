b69248e365e6e48c6f2b05aaff24b017b4ede95a
garrach <garrach76@gmail.com>
2024-06-18 21:58:59 +0100

    authLR

--- functions/server.js
+++ functions/server.js
@@ -8,15 +8,29 @@ app.use(cors());
 app.use(express.json());
 
 const router = express.Router();
-
+const regClient=[]
 router.get('/', (req, res) => {
     res.json({
         hello: 'world'
     });
 });
 app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, '../public/login.html'));
+})
+
+app.post('/api/auth/register',(req,res)=>{
+    regClient.push(req.body);
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
+app.post('/api/auth/login',(req,res)=>{
+    if (regClient.length>0) {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    }else{
+        res.send(JSON.stringify({response:"no reg found"}))
+    }
+})
+
+
 app.use('/.netlify/functions/server', router); // route must match the redirect in netlify.toml
 
 const server = require('http').createServer(app);
