f3020a863ce1adf6791d2043ef58262f98b8b38e
garrach <garrach76@gmail.com>
2024-06-18 22:22:03 +0100

    sumr

--- functions/server.js
+++ functions/server.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 const cors = require('cors');
 const serverless = require('serverless-http');
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const router = express.Router();
 const regClient=[]
-
+router.get('/', (req, res) => {
+    res.json({
+        hello: 'world'
+    });
+});
 app.get('/',(req,res)=>{
-    if (regClient.length>0) {
-        res.sendFile(path.join(__dirname, '../index.html'));
-    }else{
-        regClient.push(req.body);
-        res.send(`<a href="https://chachaxocode76.netlify.app/">LOGIN</a>`)
-    }
+   
 })
+
 app.post('/api/auth/register',(req,res)=>{
     regClient.push(req.body);
     res.sendFile(path.join(__dirname, '../public/login.html'));
@@ -26,5 +29,18 @@ app.post('/api/auth/login',(req,res)=>{
         res.send(JSON.stringify({response:"no reg found"}))
     }
 })
+
+
+app.use('/.netlify/functions/server', router); // route must match the redirect in netlify.toml
+
+const server = require('http').createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log(`Received message => ${message}`);
+    });
+    ws.send('Hello! Message From Server!!');
+});
 
 module.exports.handler = serverless(app);
