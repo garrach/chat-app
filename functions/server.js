deb584c9e7c565ba55b3e8b9cac35b4f06a07d92
garrach <garrach76@gmail.com>
2024-06-18 08:14:39 +0100

    wsandser

--- functions/server.js
+++ functions/server.js
@@ -6,9 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -21,7 +18,14 @@ app.get('/',(req,res)=>{
 })
 app.use('/.netlify/functions/server', router); // route must match the redirect in netlify.toml
 
-server.listen(3030, () => {
-    console.log(`Matchmaking Front-end listening at http://localhost:3030`);
+const server = require('http').createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log(`Received message => ${message}`);
+    });
+    ws.send('Hello! Message From Server!!');
 });
+
 module.exports.handler = serverless(app);
