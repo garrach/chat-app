27b351c63388c75f22dbc7c1fb086b07f63a5e19
garrach <garrach76@gmail.com>
2024-06-18 08:09:46 +0100

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 const serverless = require('serverless-http');
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -13,4 +21,7 @@ app.get('/',(req,res)=>{
 })
 app.use('/.netlify/functions/server', router); // route must match the redirect in netlify.toml
 
+server.listen(3030, () => {
+    console.log(`Matchmaking Front-end listening at http://localhost:3030`);
+});
 module.exports.handler = serverless(app);
